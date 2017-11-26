3437350bb55e1d4ed4844fbefa9ce11e97b571c4
rjramos70 <rjramos70@gmail.com>
2017-11-26 20:51:14 -0200

    Update the class Value just to remove warnings

--- 7SnakeProblem/src/com/ascent_software/model/Value.java
+++ 7SnakeProblem/src/com/ascent_software/model/Value.java
@@ -1,13 +1,5 @@
 package com.ascent_software.model;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
-
 public class Value{
 
 	protected int row;
